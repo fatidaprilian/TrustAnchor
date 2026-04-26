2dff7b2aded3522e5ea98c70e12b421779101151
fatidaprilian <fatidaprilian@users.noreply.github.com>
2026-04-26 19:18:36 +0700

    fix: add webpack alias for production build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
+import path from "node:path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
   experimental: {
     typedRoutes: true
+  },
+  webpack(config) {
+    config.resolve.alias["@"] = path.resolve(process.cwd(), "src");
+    return config;
   }
 };
 
